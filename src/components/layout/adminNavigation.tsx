f7b3ff47c165710a6afc0e54426d9c61587822d9
DUCANHGHEL <aduc7246@gmail.com>
2026-06-13 10:10:54 +0700

    Update adminNavigation.tsx

--- src/components/layout/adminNavigation.tsx
+++ src/components/layout/adminNavigation.tsx
@@ -2,10 +2,10 @@ import { Home, Image, Package, Settings, ShoppingCart, Tag, UsersRound } from 'l
 
 export const adminNavItems = [
   { name: 'Tổng quan', url: '/admin', icon: Home, end: true },
+  { name: 'Đơn hàng', url: '/admin/orders', icon: ShoppingCart },
   { name: 'Sản phẩm', url: '/admin/products', icon: Package },
   { name: 'Danh mục', url: '/admin/categories', icon: Tag },
   { name: 'Banner', url: '/admin/home-content', icon: Image },
-  { name: 'Đơn hàng', url: '/admin/orders', icon: ShoppingCart },
   { name: 'Tài khoản', url: '/admin/accounts', icon: UsersRound },
   { name: 'Cài đặt', url: '/admin/settings', icon: Settings },
 ];
